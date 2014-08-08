528fca9c29eda28a04ef1ce658d1a7840f7f5e95
d9magai <kumagai@sencorp.co.jp>
2014-08-08 17:37:51 +0900

    fix option

--- recipes/tool_install.rb
+++ recipes/tool_install.rb
@@ -8,7 +8,7 @@ tool_path = "#{node['ec2-metadata']['install_dir']}/#{node['ec2-metadata']['tool
 
 execute "install #{node['ec2-metadata']['toolname']}" do
   command <<-EOH
-    wget #{node['ec2-metadata']['download_url']} -P #{node['ec2-metadata']['install_dir']} -O #{node['ec2-metadata']['toolname']}
+    wget #{node['ec2-metadata']['download_url']} -O #{tool_path}
     chmod +x "#{tool_path}"
   EOH
   not_if { File.exist?(tool_path) }
