c79c3ff6f0650649ec20e518d2333104158e2439
d9magai <kumagai@sencorp.co.jp>
2014-08-08 16:53:09 +0900

    fix toolname

--- recipes/default.rb
+++ recipes/default.rb
@@ -10,7 +10,7 @@ end
 
 tool_path = "#{default['ec2metadata']['install_dir']}/#{default['ec2metadata']['toolname']}"
 
-execute "install ec2-metadata" do
+execute "install #{default['ec2metadata']['toolname']}" do
   command <<-EOH
     wget http://s3.amazonaws.com/ec2metadata/ec2-metadata -P #{default['ec2metadata']['install_dir']} -O #{default['ec2metadata']['toolname']}
     chmod +x "#{tool_path}"
