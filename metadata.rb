c16ea933636d4289ce8cc537e7219beb28ea7f74
d9magai <kumagai@sencorp.co.jp>
2014-08-08 16:40:06 +0900

    add metadata

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'ec2metadata'
+maintainer       'd9magai'
+license          'All rights reserved'
+description      'Installs/Configures ec2metadata'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
