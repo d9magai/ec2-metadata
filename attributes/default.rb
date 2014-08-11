6c07ada5b06888fffb86ad4107702f2243fa6ef8
d9magai <kumagai@sencorp.co.jp>
2014-08-11 19:26:30 +0900

    add userdata_toolname to attribute

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,3 +2,4 @@
 default['ec2-metadata']['download_url'] = 'http://s3.amazonaws.com/ec2metadata/ec2-metadata'
 default['ec2-metadata']['install_dir'] = '/usr/local/bin'
 default['ec2-metadata']['toolname'] = 'ec2-metadata'
+default['ec2-metadata']['userdata_toolname'] = 'ec2-userdata'
