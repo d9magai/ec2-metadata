f6a29fc5c7f2a92e112027bce6cacd9df3e8192d
d9magai <kumagai@sencorp.co.jp>
2014-09-01 11:43:41 +0900

    mode 0755

--- recipes/tool_install.rb
+++ recipes/tool_install.rb
@@ -8,6 +8,6 @@ tool_path = "#{node['ec2-metadata']['install_dir']}/#{node['ec2-metadata']['tool
 remote_file "#{tool_path}" do
   source node['ec2-metadata']['download_url']
   checksum node['ec2-metadata']['checksum']
-  mode "0777"
+  mode "0755"
   not_if { File.exist?(tool_path) }
 end
