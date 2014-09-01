d9965b456a67191eb616994f159429d13e7d2350
d9magai <kumagai@sencorp.co.jp>
2014-09-01 11:43:26 +0900

    use jq

--- recipes/ec2-userdata.rb
+++ recipes/ec2-userdata.rb
@@ -5,6 +5,13 @@
 # install ec2-userdata
 #
 
+include_recipe "yum-epel::default"
+OPTIONS="--enablerepo=epel"
+package 'jq' do
+  options OPTIONS
+  action :install
+end
+
 template "#{node['ec2-metadata']['install_dir']}/#{node['ec2-metadata']['userdata_toolname']}" do
   source "ec2-userdata.erb"
   owner "root"
